5cfedc117295b4b32a68846a86103187ffe052e9
ChangjinKoo <cjkoo96@gmail.com>
2021-03-23 22:28:19 +0900

    commit kalman_filter.py

--- code/week-3/EKF/kalman_filter.py
+++ code/week-3/EKF/kalman_filter.py
@@ -2,6 +2,7 @@ import numpy as np
 from math import sqrt
 from math import atan2
 from tools import Jacobian
+from math import pi
 
 class KalmanFilter:
     def __init__(self, x_in, P_in, F_in, H_in, R_in, Q_in):
@@ -19,6 +20,7 @@ class KalmanFilter:
     def update(self, z):
         S = np.dot(np.dot(self.H, self.P), self.H.T) + self.R
         K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
+        
         # Calculate new estimates
         self.x = self.x + np.dot(K, z - np.dot(self.H, self.x))
         self.P = self.P - np.dot(np.dot(K, self.H), self.P)
@@ -27,9 +29,18 @@ class KalmanFilter:
         # TODO: Implement EKF update for radar measurements
         # 1. Compute Jacobian Matrix H_j
         # 2. Calculate S = H_j * P' * H_j^T + R
-        # 3. Calculate Kalman gain K = H_j * P' * Hj^T + R
+        # 3. Calculate Kalman gain K = H_j * P' * H_j^T + R
         # 4. Estimate y = z - h(x')
         # 5. Normalize phi so that it is between -PI and +PI
         # 6. Calculate new estimates
         #    x = x' + K * y
         #    P = (I - K * H_j) * P
+        px, py, vx, vy = self.x
+        H_j = Jacobian(self.x)
+        S = np.dot(np.dot(H_j, self.P), H_j.T) + self.R
+        K = np.dot(np.dot(self.P, H_j.T), np.linalg.inv(S))
+        y = z - [sqrt(px*px+py*py), atan2(py,px), (px*vx+py*vy)/sqrt(px*px+py*py)]
+        while (y[1] > pi): y[1] -= 2*pi
+        while (y[1] < -pi): y[1] += 2*pi
+        self.x = self.x + np.dot(K, y)
+        self.P = self.P - np.dot(np.dot(K, H_j), self.P)
